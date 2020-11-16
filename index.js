c62eb9514353e7b0e05f0f14468bfb627f0753b0
sschneeberg <simone.m.schneeberg@gmail.com>
2020-11-16 14:58:19 -0500

    Set up ejs

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'ejs')
+
 app.get('/', function(req, res) {
-    res.send('faves/hates app');
+    res.render('index');
 })
 
 
